1c797b24f5f74e739f16c4289d43624a3f5b8654
Ivan <ivan.mendoza@carvana.com>
2024-06-20 16:26:36 -0600

    Updated for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/onelap-redesign/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/onelap-redesign" : "",
+};
 
 export default nextConfig;
