2fdb0c5c77b44a4222758d2d16341f8c7ebda9de
Ivan <ivan.mendoza@carvana.com>
2024-06-20 16:36:25 -0600

    fix: push changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/onelap-redesign/" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/onelap-redesign" : "",
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/onelap-redesign/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/onelap-redesign' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
